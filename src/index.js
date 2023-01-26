58e7338757747116569b891d562fb1679c4049dc
Anna-Veresk <Anna-Vladi-2017@yandex.by>
2023-01-27 01:06:32 +0300

    add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false;
+  const arrayBrackets = [];
+  const arrayIndex = [];
+
+  bracketsConfig.forEach((typeBrackets) => {
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === '|' && typeBrackets[0] === str[i] && !arrayBrackets.includes('|')) {
+        arrayBrackets.push(str[i]);
+        arrayIndex.push(i);
+      }
+      else if (str[i] === '7' && typeBrackets[0] === str[i] && !arrayBrackets.includes('7')) {
+        arrayBrackets.push(str[i]);
+        arrayIndex.push(i);
+      }
+      else if (str[i] === '8' && typeBrackets[0] === str[i] && !arrayBrackets.includes('8')) {
+        arrayBrackets.push(str[i]);
+        arrayIndex.push(i);
+      }
+      else if (str[i] !== '|' && str[i] !== '7' && str[i] !== '8' && typeBrackets[0] === str[i]) {
+        arrayBrackets.push(str[i]);
+        arrayIndex.push(i);
+      } else {
+        let endItem = arrayBrackets[arrayBrackets.length-1];
+        if (!!arrayBrackets.length && typeBrackets.includes(endItem) && str[i] === typeBrackets[1]) {
+          if ((i - arrayIndex[arrayIndex.length - 1]) % 2 !== 0) {
+            arrayBrackets.pop();
+            arrayIndex.pop();
+          }
+        }
+      }
+    }
+  });
+  console.log(arrayBrackets);
+  return !arrayBrackets.length;
 }
